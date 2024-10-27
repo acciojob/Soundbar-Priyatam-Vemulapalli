e94a3e10ab0ae6a6cbf842a0b97c43590af2027b
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-27 10:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let currentAudio;
+
+function playSound(soundName) {
+  stopSound();
+  currentAudio = new Audio(`./sounds/${soundName}.mp3`);
+  currentAudio.play();
+}
+
+function stopSound() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+}
